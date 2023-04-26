3701f9237b7e4ed774709352b5a09d71b27934f7
iiitvamsi <bendalamkrishnavamsi@gmail.com>
2023-04-26 20:06:33 +0530

    Create, Update and delete methods implemented

--- main.go
+++ main.go
@@ -28,14 +28,54 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
+	params := mux.Vars(r) //parmaters
 
 	for _, item := range movies {
 		if item.ID == params["ID"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
+
 	}
+	json.NewEncoder(w).Encode("Item not found")
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	json.NewDecoder(r.Body).Decode(&movie)
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movies)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["ID"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			json.NewDecoder(r.Body).Decode(&movie)
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movies)
+			return
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["ID"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+
 }
 
 var movies []Movie
@@ -48,6 +88,9 @@ func main() {
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movie/{ID}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{ID}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{ID}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("Starting server on 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
